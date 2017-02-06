013b3ad26c98d22989d25c4cc1f42008054d645b
Dhalton Huber <dhaltonhuber@dhaltons-mbp.mobile.asu.edu>
2017-02-06 08:29:14 -0700

    Initial Commit

---
+++ Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 2
+//
+//  Created by Dhalton Huber on 2/6/17.
+//  Copyright © 2017 RichardHuber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
